03076f909c65d75958f6b890e6fec9ffb58ec19c
Sofya Malina <sopha.malinka@yandex.ru>
2023-04-18 01:17:41 +0300

    Review 1

--- yatube_api/posts/migrations/0003_auto_20230418_0004.py
+++ yatube_api/posts/migrations/0003_auto_20230418_0004.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
             name='Follow',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('following', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='following', to=settings.AUTH_USER_MODEL)),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='following', to=settings.AUTH_USER_MODEL)),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='follower', to=settings.AUTH_USER_MODEL)),
             ],
         ),
