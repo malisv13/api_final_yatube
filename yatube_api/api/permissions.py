313f74b25088f37ddf573cf6380fa75fe067cfbb
Sofya Malina <sopha.malinka@yandex.ru>
2023-04-18 13:49:50 +0300

    Review 1

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -8,4 +8,5 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return (obj.author == request.user
+                or request.method in permissions.SAFE_METHODS)
